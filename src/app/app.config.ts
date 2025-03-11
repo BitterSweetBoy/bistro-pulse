22aa624c80e972e3e14e79f1c43853179681d9c9
Denis Dueñas <denis.mejia@cit.hn>
2025-03-10 18:20:09 -0600

    feat(ui): instalar y configurar PrimeNG con theming personalizado
    - Instala PrimeNG
    - Configura el módulo global de estilos para aplicar el tema de PrimeNG
    - Agrega Tailwind CSS para personalización adicional
    - Importa y configura el tema base en `styles.scss`
    - Ajusta variables CSS para personalizar la apariencia de los componentes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { provideNgIconLoader, withCaching } from '@ng-icons/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment.development';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || 'none'
+        }
+      }
+    }),
+    provideHttpClient(),
+    provideNgIconLoader(name => {
+      const http = inject(HttpClient);
+      return http.get(`${environment.ICONS_BASE_URL}${name}.svg`, { responseType: 'text' });
+    }, withCaching()),
+  ]
 };
